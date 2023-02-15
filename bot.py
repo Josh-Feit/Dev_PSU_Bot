fd610c8acb1ecf29b37f68563ab9406f769551c5
Josh-Feit <106037593+Josh-Feit@users.noreply.github.com>
2023-02-14 20:00:25 -0500

    Bot Basics

--- bot.py
+++ bot.py
@@ -10,5 +10,26 @@ intents.message_content = True
 
 class DevPSUBot(discord.Client):
 
-client = discord.Client(intents=intents)
+    async def on_ready(self):
+        print(f'We have logged in as {client.user}')
+
+    async def on_message(self, message):
+        print(f"Message Found: {message.content} from {message.author} in {message.channel}") 
+
+        if message.author == client.user:
+            return
+        
+        if "hello" in message.content.lower():
+            print(f"Command recognized: hello")
+            await message.channel.send("Hello!")
+
+        if client.user in message.mentions:
+            print("Bot mentioned")
+            await message.channel.send("I was mentioned!")
+
+    async def on_typing(self, channel, user, when):
+        print(f"{user} is typing in {channel} at {when}")
+        await channel.send(f"I see you typing {user}")
+        
+client = DevPSUBot(intents=intents)
 client.run(token)
